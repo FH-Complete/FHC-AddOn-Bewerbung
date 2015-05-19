419d5af463abaf2eb5900035e842e185a0d2ec0c
Nikolaus Krondraf <krondraf@technikum-wien.at>
2015-05-19 13:26:05 +0200

    Add missing phrases

--- locale/en-US/bewerbung.php
+++ locale/en-US/bewerbung.php
@@ -4,63 +4,63 @@ $this->phrasen['bewerbung/welcome']='Welcome to the Online Application';
 $this->phrasen['bewerbung/registration']='Registration';
 $this->phrasen['bewerbung/registrieren']='Register';
 $this->phrasen['bewerbung/registrierenOderZugangscode']='<a href="'.$_SERVER['PHP_SELF'].'?method=registration">Register here</a> or enter access code';
-$this->phrasen['bewerbung/einleitungstext']='Für eine Bewerbung füllen Sie bitte das Formular <u>vollständig</u> aus und klicken Sie anschließend auf "Registrieren". <br>
-												Danach haben Sie noch die Möglichkeit, weitere Daten einzugeben und Dokumente hochzuladen, bevor Sie Ihre Bewerbung abschicken.';
+$this->phrasen['bewerbung/einleitungstext']='To apply please fill out the form <u>completely</u> and click on "Register".<br>
+                                                Afterwards you will still have the possibility to enter additional information and upload documents before submitting your application.';
 $this->phrasen['bewerbung/login']='Login';
 $this->phrasen['bewerbung/zugangscode']='Access Code';
 $this->phrasen['bewerbung/fallsVorhanden']='(if available)';
-$this->phrasen['bewerbung/mailtextHtml']='Please display this message as HTML to see the complete link.';
-$this->phrasen['bewerbung/mailtext']='Dear Mr/Mrs %1$s %2$s.<br><br>
-        Thank you for your interest on a degree program of the '.CAMPUS_NAME.'. <br> 
-        To apply for a degree program please use the following link and access code: <br><br>
-        <a href="'.APP_ROOT.'cis/public/registration.php">Link to registration</a><br>
-        Access Code: %3$s <br><br>
-        Best regards, <br>
-        '.CAMPUS_NAME;
-$this->phrasen['bewerbung/zugangsdatenFalsch']='The access code is not valid or you are not registered yet.';
+$this->phrasen['bewerbung/mailtextHtml']='Please look at the message in the HTML view, in order to display the link fully.';
+$this->phrasen['bewerbung/mailtext']='Dear Mr / Ms %1$s %2$s.<br><br>
+                                        Thank you for your interest in a degree program at '.CAMPUS_NAME.'. <br>
+                                        To apply for a course, please use the following link and access code:<br><br>
+                                        <a href="'.APP_ROOT.'cis/public/registration.php">Link to registration</a><br>
+                                        Access code:%3$s <br><br>
+                                        Best regards, <br>
+                                        '.CAMPUS_NAME;
+$this->phrasen['bewerbung/zugangsdatenFalsch']='The access code you have entered is incorrect or you have not yet registered.';
 $this->phrasen['bewerbung/fehlerBeimSenden']='An error occured while sending the E-Mail.';
 $this->phrasen['bewerbung/zurueckZurAnmeldung']='Back to registration.';
-$this->phrasen['bewerbung/sicherheitscodeFalsch']='The access code is not valid.';
-$this->phrasen['bewerbung/geplanterStudienbeginn']='Planned Starting Date';
-$this->phrasen['bewerbung/studienrichtung']='Desired Degree Program(s)';
-$this->phrasen['bewerbung/bitteStudienrichtungWaehlen']='Please tick one degree program.';
+$this->phrasen['bewerbung/sicherheitscodeFalsch']='The access code is incorrect.';
+$this->phrasen['bewerbung/geplanterStudienbeginn']='Planned start of study period';
+$this->phrasen['bewerbung/studienrichtung']='Desired field of study(-ies)';
+$this->phrasen['bewerbung/bitteStudienrichtungWaehlen']='Please select at least one field of study.';
 $this->phrasen['bewerbung/bitteVornameAngeben']='Please enter your first name.';
 $this->phrasen['bewerbung/bitteNachnameAngeben']='Please enter your last name.';
 $this->phrasen['bewerbung/bitteGeburtsdatumEintragen']='Please enter your date of birth.';
 $this->phrasen['bewerbung/bitteGeschlechtWaehlen']='Please enter your gender.';
 $this->phrasen['bewerbung/bitteEmailAngeben']='Please enter a valid E-Mail Adress.';
-$this->phrasen['bewerbung/bitteStudienbeginnWaehlen']='Bitte wählen Sie den gewünschten Studienbeginn.';
-$this->phrasen['bewerbung/captcha']='Please enter the text as seen in the picture (spam protection).';
+$this->phrasen['bewerbung/bitteStudienbeginnWaehlen']='Please select the desired start of study period.';
+$this->phrasen['bewerbung/captcha']='Please enter the characters shown in the picture (spam protection).';
 $this->phrasen['bewerbung/andereGrafik']='Reload picture';
 $this->phrasen['bewerbung/datumFormat']='dd.mm.yyyy';
 $this->phrasen['bewerbung/egal']='no preference';
-$this->phrasen['bewerbung/orgform']='Organisationsform';
+$this->phrasen['bewerbung/orgform']='Organizational structure';
 $this->phrasen['bewerbung/orgform/berufsbegleitend']='extra-occupational';
 $this->phrasen['bewerbung/orgform/vollzeit']='Full time';
 $this->phrasen['bewerbung/orgform/teilzeit']='Part time';
 $this->phrasen['bewerbung/orgform/distance']='Distance Study';
 $this->phrasen['bewerbung/topprio']='Top priority';
 $this->phrasen['bewerbung/alternative']='Alternative';
-$this->phrasen['bewerbung/priowaehlen']='Select primary and alternative course of studies';
-$this->phrasen['bewerbung/prioBeschreibungstext'] = 'Please choose the type of program. If possible, choose an alternative in case it\'s fully booked.';
+$this->phrasen['bewerbung/priowaehlen']='Select primary and alternative variant';
+$this->phrasen['bewerbung/prioBeschreibungstext'] = 'Please choose the organizational structure and language. In the event that there are no more places you can also select an alternative.';
 $this->phrasen['bewerbung/prioUeberschrifttopprio'] = 'Top priority';
 $this->phrasen['bewerbung/prioUeberschriftalternative'] = 'Alternative';
-$this->phrasen['bewerbung/neuerStudiengang'] = 'Bitte wählen Sie einen Studiengang für den Sie sich bewerben möchten';
-$this->phrasen['bewerbung/geplanteStudienrichtung']='Geplante Studienrichtung';
+$this->phrasen['bewerbung/neuerStudiengang'] = 'Please choose a degree program to which you wish to apply';
+$this->phrasen['bewerbung/geplanteStudienrichtung']='Planned field of study';
 $this->phrasen['bewerbung/menuAllgemein']='General';
 $this->phrasen['bewerbung/loginmitAccount']='Login with account';
-$this->phrasen['bewerbung/allgemeineErklaerung']='Wir freuen uns dass Sie sich für einen oder mehrere unserer Studiengänge bewerben.<br><br>
-	Bitte füllen Sie das Formular vollständig aus und schicken Sie es danach ab.<br><br>
-	<b>Bewerbungsmodus:</b><br>
-	<p style="text-align:justify;">Füllen Sie alle Punkte aus. Sind alle Werte vollständig eingetragen, können Sie unter "Bewerbung abschicken" Ihre Bewerbung and die zuständige Assistenz schicken.<br>
-	Diese wird sich in den nächsten Tagen bei Ihnen melden.';
+$this->phrasen['bewerbung/allgemeineErklaerung']='We are pleased that you are applying for one or more of our programs.<br><br>
+                                                    Please fill out the form in full and then send it off.<br><br>
+                                                    <b>Application Mode:</b><br>
+                                                    <p style="text-align:justify;">Fill in all points. If all the values are entered correctly, under "Submit application" you can send your application to the administrative assistant responsible.<br>
+                                                    They will contact you in the course of the next few days.</p>';
 $this->phrasen['bewerbung/aktuelleBewerbungen']='Current applications:';
 $this->phrasen['bewerbung/status']='Status';
-$this->phrasen['bewerbung/bewerbungsstatus']='Bewerbungsstatus';
-$this->phrasen['bewerbung/keinStatus']='Noch kein Status vorhanden';
+$this->phrasen['bewerbung/bewerbungsstatus']='Application Status';
+$this->phrasen['bewerbung/keinStatus']='Still no status available';
 $this->phrasen['bewerbung/bestaetigt']='confirmed';
 $this->phrasen['bewerbung/nichtBestaetigt']='not yet confirmed';
-$this->phrasen['bewerbung/studiengangHinzufuegen']='Add Degree-Program';
+$this->phrasen['bewerbung/studiengangHinzufuegen']='Add degree program';
 $this->phrasen['bewerbung/weiter']='continue';
 $this->phrasen['bewerbung/geburtsnation']='Country of Birth';
 $this->phrasen['bewerbung/svnr']='Austrian Social Security Number';
@@ -73,71 +73,70 @@ $this->phrasen['bewerbung/artDerTaetigkeit']='Type of Occupation';
 $this->phrasen['bewerbung/weiter']='Next';
 $this->phrasen['bewerbung/eintragVom']='Date of record:';
 $this->phrasen['bewerbung/menuPersDaten']='Personal Data';
-$this->phrasen['bewerbung/accountVorhanden']='Da sie bereits einen Account haben können Sie ihre Stammdaten nicht mehr ändern. Sollten hier fehlerhafte Angaben vorhanden sein, wenden Sie sich bitte an die zuständige Assistenz<br><br>';
+$this->phrasen['bewerbung/accountVorhanden']='Since you already have an account, you can no longer change your basic data. If there are some incorrect details here, please contact the administrative assistant responsible.<br><br>';
 $this->phrasen['bewerbung/bitteAuswaehlen']='-- please select --';
 $this->phrasen['bewerbung/menuKontaktinformationen']='Contact Details';
 $this->phrasen['bewerbung/kontakt']='Contact';
 $this->phrasen['bewerbung/nation']='Nation';
 $this->phrasen['bewerbung/menuDokumente']='Documents';
-$this->phrasen['bewerbung/bitteDokumenteHochladen']='Bitte laden Sie alle vorhandenen Dokumente, die für Ihre Bewerbung relevant sind, über folgenden Link hoch:';
+$this->phrasen['bewerbung/bitteDokumenteHochladen']='Please upload all existing documents that are relevant to your application, via the following link:';
 $this->phrasen['bewerbung/linkDokumenteHochladen']='Upload Documents';
 $this->phrasen['bewerbung/dokumenteZumHochladen']='Required documents:';
 $this->phrasen['bewerbung/dokumentName']='Name';
 $this->phrasen['bewerbung/benoetigtFuer']='Required for';
 $this->phrasen['bewerbung/dokumentErforderlich']='Required document';
-$this->phrasen['bewerbung/dokumentOffen']='Dokument noch nicht abgegeben (offen)';
-$this->phrasen['bewerbung/dokumentNichtUeberprueft']='Dokument wurde abgegeben aber noch nicht überprüft';
-$this->phrasen['bewerbung/dokumentWirdNachgereicht']='Dokument wird nachgereicht';
-$this->phrasen['bewerbung/dokumentWurdeUeberprueft']='Dokument wurde bereits überprüft';
-$this->phrasen['bewerbung/abgegeben']='abgegeben';
+$this->phrasen['bewerbung/dokumentOffen']='Document not yet submitted (open)';
+$this->phrasen['bewerbung/dokumentNichtUeberprueft']='Document has been submitted but not yet examined';
+$this->phrasen['bewerbung/dokumentWirdNachgereicht']='Document will be submitted later';
+$this->phrasen['bewerbung/dokumentWurdeUeberprueft']='Document has already been examined';
+$this->phrasen['bewerbung/abgegeben']='Submitted';
 $this->phrasen['bewerbung/upload']='Upload';
-$this->phrasen['bewerbung/menuZgv']='ZGV';
-$this->phrasen['bewerbung/menuZugangsvoraussetzungen']='Zugangsvoraussetzungen';
+$this->phrasen['bewerbung/menuZgv']='Admission requirements';
+$this->phrasen['bewerbung/menuZugangsvoraussetzungen']='Admission requirements';
 $this->phrasen['bewerbung/fuer']='for';
-$this->phrasen['bewerbung/artDerVoraussetzung']='Art der Voraussetzung';
+$this->phrasen['bewerbung/artDerVoraussetzung']='Type of condition';
 $this->phrasen['bewerbung/menuZahlungen']='Payments';
-$this->phrasen['bewerbung/zahlungsinformation']='Zahlungsinformation';
+$this->phrasen['bewerbung/zahlungsinformation']='Payment information';
 $this->phrasen['bewerbung/bezahlt']='payed';
-$this->phrasen['bewerbung/zahlungsdetails']='Zahlungsdetails';
-$this->phrasen['bewerbung/menuReihungstest']='Reihungstest';
-$this->phrasen['bewerbung/fuerReihungstestAnmelden']='Sie können sich für folgende Reihungstest anmelden:';
+$this->phrasen['bewerbung/zahlungsdetails']='Payment details';
+$this->phrasen['bewerbung/menuReihungstest']='Assessment test';
+$this->phrasen['bewerbung/fuerReihungstestAnmelden']='You can register for the following assessment test:';
 $this->phrasen['bewerbung/fehler']='An error occurred';
-$this->phrasen['bewerbung/angemeldetPlaetze']='angemeldet / Plätze';
+$this->phrasen['bewerbung/angemeldetPlaetze']='registered / places';
 $this->phrasen['bewerbung/uhrzeit']='Time';
 $this->phrasen['bewerbung/stornieren']='cancel';
 $this->phrasen['bewerbung/menuBewerbungAbschicken']='Send Application';
-$this->phrasen['bewerbung/erklaerungBewerbungAbschicken']='Haben Sie alle Daten korrekt ausgefüllt bzw. alle Dokumente auf das System hochgeladen, können Sie Ihre Bewerbung abschicken.<br>
-		Die jeweilige Studiengangsassistenz wird sich in den folgenden Tagen, bezüglich der Bewerbung, bei Ihnen Melden.
-		<br><br>
-		Bitte überprüfen Sie nochmals Ihre Daten.<br>
-		Um Ihre Bewerbung jetzt abzuschließen klicken auf folgenden Link:';
+$this->phrasen['bewerbung/erklaerungBewerbungAbschicken']='If you have filled in all the information correctly and / or uploaded all the documents on the system, you can submit your application.<br>
+                                                               The degree program administrative assistant responsible will contact you in the course of the next few days regarding the application.<br><br>
+                                                               Please double-check your data.<br>
+                                                               To complete your application click on the following link:';
 $this->phrasen['bewerbung/bewerbungAbschickenFuer']='Send application for';
 $this->phrasen['bewerbung/buttonBewerbungAbschicken']='Send application';
 $this->phrasen['bewerbung/logout']='Logout';
 $this->phrasen['bewerbung/einzahlungFuer']='Payment from';
-$this->phrasen['bewerbung/zahlungsinformationen']='Zahlungsinformationen';
-$this->phrasen['bewerbung/buchungstyp']='Buchungstyp';
-$this->phrasen['bewerbung/buchungstext']='Buchungstext';
+$this->phrasen['bewerbung/zahlungsinformationen']='Payment information';
+$this->phrasen['bewerbung/buchungstyp']='Booking type';
+$this->phrasen['bewerbung/buchungstext']='Booking text';
 $this->phrasen['bewerbung/betrag']='Amount';
 $this->phrasen['bewerbung/zahlungAn']='Payment to';
 $this->phrasen['bewerbung/empfaenger']='Recipient';
 $this->phrasen['bewerbung/iban']='IBAN';
 $this->phrasen['bewerbung/bic']='BIC';
-$this->phrasen['bewerbung/zahlungsreferenz']='Zahlungsreferenz';
-$this->phrasen['bewerbung/buchungsnummerNichtVorhanden']='Buchungsnummer %s nicht vorhanden';
+$this->phrasen['bewerbung/zahlungsreferenz']='Payment Reference';
+$this->phrasen['bewerbung/buchungsnummerNichtVorhanden']='Booking number %s not present';
 $this->phrasen['bewerbung/vollstaendig']='complete';
 $this->phrasen['bewerbung/unvollstaendig']='incomplete';
 $this->phrasen['bewerbung/maxAnzahlTeilnehmer']='Maximum number of participants reached';
-$this->phrasen['bewerbung/erfolgreichBeworben']='Sie haben sich erfolgreich beworben. Die zuständige Assistenz wird sich in den nächsten Tagen bei Ihnen melden.';
+$this->phrasen['bewerbung/erfolgreichBeworben']='You have applied successfully. The administrative assistant responsible will contact you in the course of the next few days.';
 $this->phrasen['bewerbung/fehlerBeimVersendenDerBewerbung']='An error occured while sending the application. Please try again.';
 $this->phrasen['bewerbung/svnrBereitsVorhanden']='Social Security Number already exists.';
-$this->phrasen['bewerbung/menuBewerbungFuerStudiengang']='Application for a Degree-Program';
+$this->phrasen['bewerbung/menuBewerbungFuerStudiengang']='Application for a degree program';
 $this->phrasen['bewerbung/emailBodyStart']='Es hat sich ein Student für Ihren Studiengang beworben. <br>';
 $this->phrasen['bewerbung/emailBodyEnde']='Für mehr Details verwenden Sie die Personenansicht im FAS.';
 $this->phrasen['bewerbung/fileUpload']='File-Upload';
 $this->phrasen['bewerbung/fehlerKeinePersonId']='There was no Person_id supplied';
-$this->phrasen['bewerbung/woWurdeUrkundeAusgestellt']='Wo wurde die Urkunde ausgestellt?';
+$this->phrasen['bewerbung/woWurdeUrkundeAusgestellt']='Where was the certificate issued?';
 $this->phrasen['bewerbung/ausbildungstyp']='Type of Education';
-$this->phrasen['bewerbung/lehrgang']='Lehrgang';
-$this->phrasen['bewerbung/keineStgAngeboten']='Es werden derzeit keine Studiengänge angeboten';
-$this->phrasen['bewerbung/keineLehrgAngeboten']='Es werden derzeit keine Lehrgänge angeboten';
+$this->phrasen['bewerbung/lehrgang']='Course';
+$this->phrasen['bewerbung/keineStgAngeboten']='There are no degree programs available at the moment';
+$this->phrasen['bewerbung/keineLehrgAngeboten']='There are no courses available at the moment';
