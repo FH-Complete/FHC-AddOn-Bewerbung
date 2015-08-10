9020bf7d3cb993463ee09e71e791804b2b81889c
oesi <oesi@technikum-wien.at>
2015-08-10 13:14:05 +0200

    Beim Upload von Lichtbildern werden diese direkt als Profilfoto bei der Person übernommen

--- cis/dms_akteupload.php
+++ cis/dms_akteupload.php
@@ -30,14 +30,15 @@ require_once('../../../include/benutzerberechtigung.class.php');
 require_once('../../../include/akte.class.php');
 require_once('../../../include/dokument.class.php');
 require_once('../../../include/mail.class.php');
-require_once('../../../include/phrasen.class.php'); 
-require_once('../../../include/dms.class.php'); 
+require_once('../../../include/phrasen.class.php');
+require_once('../../../include/dms.class.php');
+require_once('../../../include/fotostatus.class.php');
 
 header("Content-Type: text/html; charset=utf-8");
 
 session_cache_limiter('none'); //muss gesetzt werden sonst funktioniert der Download mit IE8 nicht
 session_start();
-if (!isset($_SESSION['bewerbung/user']) || $_SESSION['bewerbung/user']=='') 
+if (!isset($_SESSION['bewerbung/user']) || $_SESSION['bewerbung/user']=='')
 {
     header('registration.php?method=allgemein');
     exit;
@@ -53,11 +54,11 @@ if(!isset($_SESSION['bewerbung/personId']))
 
 if($person_id!=$_SESSION['bewerbung/personId'])
 	die($p->t('global/keineBerechtigungFuerDieseSeite'));
-	
+
 $dokumenttyp = (isset($_GET['dokumenttyp']))? $_GET['dokumenttyp'] : '';
 $kategorie_kurzbz = isset($_REQUEST['kategorie_kurzbz'])?$_REQUEST['kategorie_kurzbz']:'';
-$sprache = getSprache(); 
-$p=new phrasen($sprache); 
+$sprache = getSprache();
+$p=new phrasen($sprache);
 
 $PHP_SELF = $_SERVER['PHP_SELF'];
 echo "<html>
@@ -67,24 +68,24 @@ echo "<html>
 //Bei Upload des Bildes
 if(isset($_POST['submitbild']))
 {
-    $error = false; 
-    
+    $error = false;
+
     // dms Eintrag anlegen
     if(isset($_POST['fileupload']))
     {
-        $ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION); 
+        $ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
         $filename = uniqid();
-        $filename.=".".$ext; 
+        $filename.=".".$ext;
         $uploadfile = DMS_PATH.$filename;
 
 
-        if(move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) 
+        if(move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile))
         {
             if(!chgrp($uploadfile,'dms'))
                 echo 'CHGRP failed';
             if(!chmod($uploadfile, 0774))
                 echo 'CHMOD failed';
-            exec('sudo chown wwwrun '.$uploadfile);	
+            exec('sudo chown wwwrun '.$uploadfile);
 
             $dms = new dms();
 
@@ -101,35 +102,35 @@ if(isset($_POST['submitbild']))
             {
                 $dms_id=$dms->dms_id;
 
-            }    	
+            }
             else
             {
                 echo $p->t('global/fehlerBeimSpeichernDerDaten');
-                $error = true; 
+                $error = true;
             }
-        } 
-        else 
+        }
+        else
         {
             echo $p->t('global/dateiNichtErfolgreichHochgeladen');
-            $error = true; 
+            $error = true;
         }
     }
-    
+
 	if(isset($_FILES['file']['tmp_name']) && !$error)
 	{
 		//Extension herausfiltern
     	$ext = explode('.',$_FILES['file']['name']);
         $ext = mb_strtolower($ext[count($ext)-1]);
-		
+
 		$filename = $_FILES['file']['tmp_name'];
-		
+
 		//$fp = fopen($filename,'r');
 		//auslesen
 		//$content = fread($fp, filesize($filename));
 		//fclose($fp);
-		
+
 		$akte = new akte();
-		
+
 		if($akte->getAkten($_GET['person_id'], 'Lichtbil'))
 		{
 			if(count($akte->result)>0)
@@ -137,21 +138,21 @@ if(isset($_POST['submitbild']))
 				$akte = $akte->result[0];
 				$akte->new = false;
 			}
-			else 
+			else
 				$akte->new = true;
 		}
-		else 
+		else
 		{
 			$akte->new = true;
 		}
-		
-		$dokument = new dokument(); 
+
+		$dokument = new dokument();
 		$dokument->loadDokumenttyp($_REQUEST['dokumenttyp']);
 
 		$exts_arr = explode(".",strtolower($_FILES['file']['name']));
 		$extension = end($exts_arr);
 		$titel = '';
-		
+
 		// da nur 32 zeichen gespeichert werden dürfen, muss anhand vom typ gekürzt werden
 		if($_REQUEST['dokumenttyp']=='Lebenslf')
 			$titel = $p->t('incoming/lebenslauf').".".$extension;
@@ -160,35 +161,83 @@ if(isset($_POST['submitbild']))
 		if($_REQUEST['dokumenttyp']=='Motivat')
 			$titel = $p->t('incoming/motivationsschreiben').".".$extension;
 		if($_REQUEST['dokumenttyp']=='Zeugnis')
-			$titel = $p->t('incoming/zeugnis').".".$extension;			
+			$titel = $p->t('incoming/zeugnis').".".$extension;
 		if($_REQUEST['dokumenttyp']=='Lichtbil')
-			$titel = $p->t('incoming/lichtbild').".".$extension;					
-			
-			
+			$titel = $p->t('incoming/lichtbild').".".$extension;
+
+
 		$akte->dokument_kurzbz = $_REQUEST['dokumenttyp'];
-                $akte->bezeichnung = $_FILES['file']['name']; 
+                $akte->bezeichnung = $_FILES['file']['name'];
 		$akte->person_id = $_GET['person_id'];
 		//$akte->inhalt = base64_encode($content);
 		$akte->mimetype = $_FILES['file']['type'];
 		$akte->erstelltam = date('Y-m-d H:i:s');
 		$akte->gedruckt = false;
-		$akte->titel = $titel; 
+		$akte->titel = $titel;
 		//$akte->bezeichnung = $dokument->bezeichnung;
 		$akte->updateamum = date('Y-m-d H:i:s');
 	//	$akte->updatevon = $user;
 		$akte->insertamum = date('Y-m-d H:i:s');
-		$akte->nachgereicht = false; 
-		$akte->anmerkung = ''; 
+		$akte->nachgereicht = false;
+		$akte->anmerkung = '';
 	//	$akte->insertvon = $user;
 		$akte->uid = '';
                 $akte->dms_id = $dms_id;
 		$akte->new = true;
 
 
-        if (!$akte->save()) {
+        if (!$akte->save())
+        {
             echo "<b>" . $p->t('global/fehleraufgetreten') . ": $akte->errormsg</b>";
-        } else {
+        }
+        else
+        {
             echo "<b>" . $p->t('global/erfolgreichgespeichert') . "</b>";
+            if($_REQUEST['dokumenttyp']=='Lichtbil')
+            {
+                // Wenn ein Foto hochgeladen wird dieses auch in die Person speichern
+                //groesse auf maximal 101x130 begrenzen
+    			$tempname = resize($uploadfile, 101, 130);
+
+    			//in DB speichern
+    			//File oeffnen
+    			$fp = fopen($tempname,'r');
+    			//auslesen
+    			$content = fread($fp, filesize($tempname));
+    			fclose($fp);
+                unset($tempname);
+    			//in base64 umrechnen
+    			$content = base64_encode($content);
+
+    			$person = new person();
+    			if($person->load($_GET['person_id']))
+    			{
+    				//base64 Wert in die Datenbank speichern
+    				$person->foto = $content;
+    				$person->new = false;
+    				if($person->save())
+    				{
+    					$fs = new fotostatus();
+    					$fs->person_id=$person->person_id;
+    					$fs->fotostatus_kurzbz='hochgeladen';
+    					$fs->datum = date('Y-m-d');
+    					$fs->insertamum = date('Y-m-d H:i:s');
+    					$fs->insertvon = 'bewerbertool';
+    					$fs->updateamum = date('Y-m-d H:i:s');
+    					$fs->updatevon = 'bewerbertool';
+    					if(!$fs->save(true))
+    						echo '<span class="error">Fehler beim Setzen des Bildstatus</span>';
+    				}
+                    else
+                    {
+                        echo '<span class="error">Fehler beim Speichern der Person</span>';
+                    }
+                }
+                else
+                {
+                    echo '<span class="error">Personen nicht gefunden</sapn>';
+                }
+            }
         }
 
 		echo "<script>
@@ -201,8 +250,8 @@ if(isset($_POST['submitbild']))
 
 if($person_id !='')
 {
-	$dokument = new dokument(); 
-	$dokument->getAllDokumenteForPerson($person_id); 
+	$dokument = new dokument();
+	$dokument->getAllDokumenteForPerson($person_id);
 	echo "	<form method='POST' enctype='multipart/form-data' action='$PHP_SELF?person_id=".$_GET['person_id']."'>
 			<table>
 				<tr>
@@ -218,9 +267,9 @@ if($person_id !='')
 				foreach ($dokument->result as $dok)
 				{
                                     $selected=($dokumenttyp == $dok->dokument_kurzbz)?'selected':'';
-					
+
                     echo '<option '.$selected.' value="'.$dok->dokument_kurzbz.'" >'.$dok->bezeichnung_mehrsprachig[$sprache]."</option>\n";
-					
+
 				}
 	echo "			</select>
 					</td>
@@ -230,7 +279,7 @@ if($person_id !='')
 					<td><input type='hidden' name='kategorie_kurzbz' id='kategorie_kurzbz' value='Akte'>
                     <td><input type='hidden' name='fileupload' id='fileupload'></td>
 					<td><input type='submit' name='submitbild' value='Upload'></td>
-                    
+
 				</tr>
 			</table>
 			</form>";
@@ -239,6 +288,42 @@ if($person_id !='')
 else
 {
 	echo $p->t('bewerbung/fehlerKeinePersonId');
+}
+
+function resize($filename, $width, $height)
+{
+	$ext = explode('.',$_FILES['file']['name']);
+    $ext = mb_strtolower($ext[count($ext)-1]);
+
+	// Hoehe und Breite neu berechnen
+	list($width_orig, $height_orig) = getimagesize($filename);
+
+	if ($width && ($width_orig < $height_orig))
+	{
+	   $width = ($height / $height_orig) * $width_orig;
+	}
+	else
+	{
+	   $height = ($width / $width_orig) * $height_orig;
+	}
+
+	$image_p = imagecreatetruecolor($width, $height);
+
+	$image = imagecreatefromjpeg($filename);
+
+	//Bild nur verkleinern aber nicht vergroessern
+	if($width_orig>$width || $height_orig>$height)
+		imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
+	else
+		$image_p = $image;
+
+    $tmpfname = tempnam(sys_get_temp_dir(), 'FHC');
+
+	imagejpeg($image_p, $tmpfname, 80);
+
+    imagedestroy($image_p);
+	@imagedestroy($image);
+    return $tmpfname;
 }
 
 ?>
