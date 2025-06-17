9c3d0b86f6ebf25d6719a8d8ffcf5a08f7f92f2c
Alexei Karpenko <karpen_ko@hotmail.com>
2025-06-17 14:01:59 +0200

    changed config entry name for electronig onboarding fields

--- bewerbung.config-default.inc.php
+++ bewerbung.config-default.inc.php
@@ -117,7 +117,7 @@ define('BEWERBERTOOL_DONT_SEND_MAIL_STG', serialize(array()));
 define('BEWERBERTOOL_ELECTRONIC_ONBOARDING_REGISTRATION_LINK', 'index.ci.php/extensions/FHC-Core-ElectronicOnboarding/OnboardingRegistrierung/startOnboarding');
 
 // Felder, die beim Electronic Onboarding Login vorbefüllt sind und daher nicht manuell änderbar sind
-define('BEWERBERTOOL_ELECTRONIC_ONBOARDING_VORBEFUELLTE_FELDER', array(
+define('BEWERBERTOOL_ELECTRONIC_ONBOARDING_VORBEFUELLTE_PERSON_FELDER', array(
 		'vorname',
 		'nachname',
 		'geburtsdatum',
